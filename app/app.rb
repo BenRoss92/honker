28b7720eff277e42f7873c34bdc06eff7ced1cc9
BenRoss92 <benjaminjackross@gmail.com>
2016-12-29 01:23:23 +0000

    Retains sign up info after password mismatch

--- app/app.rb
+++ app/app.rb
@@ -34,15 +34,16 @@ class Honker < Sinatra::Base
   end
 
   get '/users/sign_up' do
+    @user = User.new
     erb :'users/sign_up'
   end
 
   post '/users' do
-    user = User.new(name: params[:name], username: params[:username],
+    @user = User.new(name: params[:name], username: params[:username],
      email: params[:email], password: params[:password],
      password_confirmation: params[:password_confirmation])
-     if user.save
-      session[:user_id] = user.id
+     if @user.save
+      session[:user_id] = @user.id
       redirect('/honks')
      else
       flash.now[:error] = "Password and Password Confirmation don't match"
